b22a0e0a697eef135bb2b417064c2d8e0ff7a5aa
Wana Kijiji <wanakijiji@users.noreply.github.com>
2020-11-18 13:22:07 +0900

    Add image preload function

--- dist/hover-image.js
+++ dist/hover-image.js
@@ -12,6 +12,9 @@ $.fn.hoverImage = function(options) {
     const srcActive = srcNormal.replace(/(.*)\./, '$1' + options.suffix + '.');
     const href = $(this).parent('a').attr('href');
     const url = location.href;
+    // preload
+    $('<img>').attr('src', srcActive);
+    // switch image
     if (options.self && url.includes(href)) {
       // self page
       $(this).attr('src', srcActive);
